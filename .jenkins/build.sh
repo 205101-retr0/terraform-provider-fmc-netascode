92764258bbb95c667264ae5bb9e95aacd95befcf
205101-retr0 <ande.teja@learner.manipal.edu>
2024-02-26 13:09:54 +0530

    Updated Jenkins 4699

--- .jenkins/build.sh
+++ .jenkins/build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-jenkinsHome = "${JENKINS_HOME}"
-rm -rf [jenkinsHome]/\.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64
+jenkinsHome="${JENKINS_HOME}"
+rm -rf $jenkinsHome/\.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64
 rm -rf vendor
 
 go mod vendor
 go mod tidy
 go build -o terraform-provider-fmc_0.2_darwin_amd64
 
-mkdir -p [jenkinsHome]/\.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64
-mv terraform-provider-fmc_0.2_darwin_amd64 [jenkinsHome]/.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64
-cp [jenkinsHome]/\.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64/terraform-provider-fmc_0.2_darwin_amd64 [jenkinsHome]/.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64/terraform-provider-fmc
+mkdir -p $jenkinsHome/\.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64
+mv terraform-provider-fmc_0.2_darwin_amd64 $jenkinsHome/.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64
+cp $jenkinsHome/\.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64/terraform-provider-fmc_0.2_darwin_amd64 $jenkinsHome/.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64/terraform-provider-fmc
 
-rm [jenkinsHome]/\.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64/terraform-provider-fmc_0.2_darwin_amd64
+rm $jenkinsHome/\.terraform.d/plugins/registry.terraform.io/netascode/fmc/0.2/darwin_amd64/terraform-provider-fmc_0.2_darwin_amd64
